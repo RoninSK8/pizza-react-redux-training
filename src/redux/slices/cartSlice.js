e2ea3e98de1833a7059895dec1fd8facb113b4a1
Сергей Политаев <ronin@sonikelf.ru>
2022-06-15 23:29:20 +0300

    add cart item component and adding items to cart function

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -10,20 +10,50 @@ const cartSlice = createSlice({
 	initialState,
 	reducers: {
 		addItem(state, { payload }) {
-			const existingItem = state.items.find(
+			const currentItem = state.items.find(
 				(item) =>
 					item.id === payload.id &&
 					item.size === payload.size &&
 					item.type === payload.type
 			);
-			if (existingItem) {
-				existingItem.count += 1;
+			if (currentItem) {
+				currentItem.count += 1;
 			} else {
 				state.items.push(payload);
 			}
 		},
+		removeItem(state, { payload }) {
+			const currentItem = state.items.find(
+				(item) =>
+					item.id === payload.id &&
+					item.size === payload.size &&
+					item.type === payload.type
+			);
+			if (currentItem.count > 1) {
+				currentItem.count -= 1;
+			} else {
+				state.items = state.items.filter(
+					(item) =>
+						item.id !== payload.id ||
+						item.size !== payload.size ||
+						item.type !== payload.type
+				);
+			}
+		},
+		removeCategory(state, { payload }) {
+			state.items = state.items.filter(
+				(item) =>
+					item.id !== payload.id ||
+					item.size !== payload.size ||
+					item.type !== payload.type
+			);
+		},
+		clearCart(state) {
+			state.items = [];
+		},
 	},
 });
 
-export const { addItem } = cartSlice.actions;
+export const { addItem, removeItem, removeCategory, clearCart } =
+	cartSlice.actions;
 export default cartSlice.reducer;
